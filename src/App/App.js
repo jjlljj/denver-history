7844a9b916e6a2578a72f24cf3895a54511bb9af
James Logue <logueja@gmail.com>
2018-04-06 11:59:07 -0600

    Render 3d denver map

--- src/App/App.js
+++ src/App/App.js
@@ -8,7 +8,6 @@ class App extends Component {
     return (
       <div>
         <Map />
-        <Vrscene />
       </div>
     );
   }
--- src/Map/Map.js
+++ src/Map/Map.js
@@ -20,8 +20,57 @@ export default class Map extends Component {
     mapboxgl.accessToken = MB_TOKEN
     this.map = new mapboxgl.Map({
       container: this.mapContainer,
-      style: 'mapbox://styles/mapbox/streets-v9'
-    });
+      style: 'mapbox://styles/mapbox/light-v9',
+      center: [-104.9987887, 39.7508047],
+      zoom: 16,
+      bearing: -17.6,
+      pitch: 45
+    })
+    
+    this.map.addControl(new mapboxgl.ScaleControl({
+     maxWidth: 80,
+     unit: 'imperial'
+    }));
+    const map = this.map
+      map.on('load', function() {
+      // Insert the layer beneath any symbol layer.
+      var layers = map.getStyle().layers;
+
+      var labelLayerId;
+      for (var i = 0; i < layers.length; i++) {
+          if (layers[i].type === 'symbol' && layers[i].layout['text-field']) {
+              labelLayerId = layers[i].id;
+              break;
+          }
+      }
+
+      map.addLayer({
+          'id': '3d-buildings',
+          'source': 'composite',
+          'source-layer': 'building',
+          'filter': ['==', 'extrude', 'true'],
+          'type': 'fill-extrusion',
+          'minzoom': 15,
+          'paint': {
+              'fill-extrusion-color': '#aaa',
+
+              // use an 'interpolate' expression to add a smooth transition effect to the
+              // buildings as the user zooms in
+              'fill-extrusion-height': [
+                  "interpolate", ["linear"], ["zoom"],
+                  15, 0,
+                  15.05, ["get", "height"]
+              ],
+              'fill-extrusion-base': [
+                  "interpolate", ["linear"], ["zoom"],
+                  15, 0,
+                  15.05, ["get", "min_height"]
+              ],
+              'fill-extrusion-opacity': .6
+          }
+      }, labelLayerId);
+    })
+
   }
 
   componentWillUnmount() {
