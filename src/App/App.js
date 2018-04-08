3c5451dc59fcdbcf4cfdd63d5f22ca141c7abb93
James Logue <logueja@gmail.com>
2018-04-08 12:48:19 -0600

    Fix so map and Aframe work again

--- src/App/App.js
+++ src/App/App.js
@@ -4,6 +4,11 @@ import Vrscene from '../Vrscene/Vrscene'
 import Map from '../Map/Map'
 
 class App extends Component {
+
+  handleEvent = event => {
+   console.log(event) 
+  }
+
   render() {
     return (
       <div>
--- src/Vrscene/Vrscene.js
+++ src/Vrscene/Vrscene.js
@@ -1,19 +1,18 @@
 import React, { Component } from 'react';
 import 'aframe';
 import {Entity, Scene} from 'aframe-react';
-import Map from '../Map/Map'
 
 export default class Vrscene extends Component {
   render () {
     return (
-      <div className="vr-wrap">
-        <Scene embedded>
-          <Entity geometry={{primitive: 'box'}} material={{color: 'blue', src:''}} position={{x: 0, y: 0, z: -5}} />
+      <div className="vr-wrap" id="a-target">
+        <Scene>
+          <Entity geometry={{primitive: 'box'}} material={{color: 'blue'}} position={{x: 0, y: 0, z: -5}} />
           <Entity light={{type: 'point'}}/>
           <Entity gltf-model={{src: 'virtualcity.gltf'}}/>
           <Entity text={{value: 'Hello, WebVR!'}}/>
 
-          <Entity geometry={{primitive: 'sphere'}} material={{src:''}} position={{x: -2, y: 2, z: -4}}/>
+          <Entity geometry={{primitive: 'sphere'}} material={{color: 'teal'}} position={{x: -2, y: 2, z: -4}}/>
         </Scene>
     </div>
     );
