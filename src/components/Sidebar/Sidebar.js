9bf68ab2e84b4cffef54520d47117a57f0805fcc
James Logue <logueja@gmail.com>
2018-04-16 11:45:14 -0600

    Add sidebar hide functionality

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -2,17 +2,24 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import './Sidebar.css';
+import { hideSidebar } from '../../actions'
 
 export class Sidebar extends Component {
+
+  handleHideSidebar = () => {
+    console.log('boop')
+    this.props.hideSidebar()
+  }
 
   render() {
     const { ldmk_name, year_built, address_line1, photo_link, description } = this.props.building
     const { showSidebar } = this.props
-    const displayStatus = showSidebar ? '' : 'sidebar-hidden'
+    const displayStatus = showSidebar ? 'sidebar-show' : 'sidebar-hidden'
 
     return (
       <section className={`sidebar ${displayStatus}`} >
         <div className="banner-img-wrap">
+          <div className="hide-sidebar-btn" onClick={this.handleHideSidebar} >{'<<'}</div>
           <img src={`http://denver-history.herokuapp.com/images/${photo_link}`} />
         </div>
         <div className="content-wrap">
@@ -33,4 +40,8 @@ const mapStateToProps = ({ building, showSidebar }) => ({
   showSidebar
 })
 
-export default connect(mapStateToProps, null)(Sidebar)
+const mapDispatchToProps = (dispatch) => ({
+  hideSidebar: () => dispatch(hideSidebar())
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(Sidebar)
