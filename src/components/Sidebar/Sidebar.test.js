d18b8746302662a29c425b7cff6b5d467759e7c2
James Logue <logueja@gmail.com>
2018-04-16 16:22:04 -0600

    Add sidebar tests

--- src/components/Sidebar/Sidebar.test.js
+++ src/components/Sidebar/Sidebar.test.js
@@ -2,15 +2,34 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import { Sidebar } from './Sidebar';
+import { mockBuilding } from '../../__mocks__/mockData';
 
 describe('Sidebar', () => {
+  let renderedComponent
+  let building
+  let hideSidebar
+
+  beforeEach(() => {
+    building = mockBuilding
+    hideSidebar = jest.fn()
+    renderedComponent = shallow(<Sidebar building={ building } showSidebar={true} hideSidebar={ hideSidebar }/>)
+  })
 
   it('Should match snapshot', () => {
-    const renderedComponent = shallow(<Sidebar />)
-    //expect(renderedComponent).toMatchSnapshot()
+    expect(renderedComponent).toMatchSnapshot()
   });
 
-  it('Should not pass', () => {
-    expect(false).toEqual(true)
+  it('Should match snapshot when hidden', () => {
+    renderedComponent = shallow(<Sidebar building={ building } showSidebar={false} hideSidebar={ hideSidebar }/>)
+
+    expect(renderedComponent).toMatchSnapshot()
+  })
+
+  it('handleHideSidebar should call hideSidebar', () => {
+    expect(hideSidebar).not.toHaveBeenCalled()
+
+    renderedComponent.instance().handleHideSidebar()
+
+    expect(hideSidebar).toHaveBeenCalled()
   })
 })
