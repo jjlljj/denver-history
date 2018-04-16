26cebecf05e2270a15f97ef41e8a850b12814a01
James Logue <logueja@gmail.com>
2018-04-16 16:16:14 -0600

    Add Tour component tests

--- src/components/Tour/Tour.test.js
+++ src/components/Tour/Tour.test.js
@@ -5,11 +5,43 @@ import { shallow } from 'enzyme';
 import { Tour } from './Tour';
 jest.mock('../../../node_modules/mapbox-gl/dist/mapbox-gl', () => () => ({ Map: () => ({}) }));
 jest.mock('../../../node_modules/threebox/', () => () => ({ threebox: jest.fn() }));
+jest.mock('../../actions/index', () => () => ({ addDistrict: jest.fn() }));
+import { mockDistrict } from '../../__mocks__/mockdata'
 
 describe('Tour', () => {
+  let renderedComponent
+  let district
+  let addDistrict
+
+  beforeEach(() => {
+    district = []
+    addDistrict = jest.fn()
+    renderedComponent = shallow(<Tour district={ district } addDistrict={addDistrict}/>, { disableLifecycleMethods: true })
+
+    window.fetch = jest.fn().mockImplementation(() => Promise.resolve({
+      status: 200,
+      json: () => Promise.resolve(mockDistrict)
+    }))
+  })
 
   it('matches snapshot', () => {
-    const renderedComponent = shallow(<Tour />)
     expect(renderedComponent).toMatchSnapshot()
   })
+
+  it('fetchBuildings should call the fetch in getDistrictBuildings', () => {
+    expect(window.fetch).not.toHaveBeenCalled()
+
+    renderedComponent.instance().fetchBuildings()
+
+    expect(window.fetch).toHaveBeenCalled()
+  })
+
+  it('fetchBuildings should call the action addDistrict', async () => {
+    expect(addDistrict).not.toHaveBeenCalled()
+
+    const called = await renderedComponent.instance().fetchBuildings()
+
+    called && expect(addDistrict).toHaveBeenCalled()
+  })
+
 })
