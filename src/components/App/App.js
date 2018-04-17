4c90c4c653301dbff9ac1ac4eedc8ed7da0e3b48
James Logue <logueja@gmail.com>
2018-04-17 11:24:29 -0600

    remove unused imports in App component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,15 +1,9 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import { Route } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import './App.css';
-import Map from '../Map/Map';
 import Splash from '../Splash/Splash';
 import Tour from '../Tour/Tour';
-import Header from '../Header/Header';
-import Sidebar from '../Sidebar/Sidebar';
-import { addDistrict } from '../../actions';
-import { getDistrictBuildings } from '../../helpers/apiHelper.js';
-import { Route,  withRouter } from 'react-router-dom';
 
 export class App extends Component {
 
