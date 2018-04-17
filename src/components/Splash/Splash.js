409eb4b8920912d52f7daaab0f593076e120c734
Melissa Wagasky <melissawagasky@gmail.com>
2018-04-17 10:10:32 -0600

    Style splash page and add loading gif framework

--- src/components/Splash/Splash.js
+++ src/components/Splash/Splash.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Buildings from '../Buildings/Buildings';
-// import './Splash.css';
+import './Splash.css';
 
 export default class Splash extends Component {
   render() {
     return (
-      <div>
-        <h1>Historic Denver SPLASH</h1>
-        <Buildings />
+      <div id="splash-page">
+        <div id="layer">
+          <h1 class="logo">Historic Denver SPLASH</h1>
+          <Buildings />
+        </div>
       </div>
     )
   }
