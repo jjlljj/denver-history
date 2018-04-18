2b831bf62ca0c210c871e641f846431e16ac82ce
Nora Gully <nora.gully@gmail.com>
2018-04-18 10:09:31 -0600

    Update Splash component

--- src/components/Splash/Splash.js
+++ src/components/Splash/Splash.js
@@ -1,16 +1,16 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import './Splash.css';
 
-export default class Splash extends Component {
-  render() {
-    return (
-      <div id="splash-page">
-        <div id="layer">
-          <h1 className="logo">Historic Denver</h1>
-          <Link to="/tour"><div id="tour-button">Enter Tour</div></Link>
-        </div>
+const Splash = () => {
+  return (
+    <div id="splash-page">
+      <div id="layer">
+        <h1 className="logo">Historic Denver</h1>
+        <Link to="/tour"><div id="tour-button">Enter Tour</div></Link>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+
+export default Splash;
