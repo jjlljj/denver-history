001860eb0ba8241c5cb2453faa522438070a8f8a
Nora Gully <nora.gully@gmail.com>
2018-04-18 10:04:43 -0600

    Update app component and tests, fix tabs

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,16 +4,13 @@ import './App.css';
 import Splash from '../Splash/Splash';
 import Tour from '../Tour/Tour';
 
-export class App extends Component {
-
-  render() {
-    return (
-      <div>
-        <Route exact path='/' component={ Splash } />
-        <Route exact path='/tour' component={ Tour } />
-      </div>
-    );
-  }
+const App = () => {
+  return (
+    <div>
+      <Route exact path='/' component={ Splash } />
+      <Route exact path='/tour' component={ Tour } />
+    </div>
+  );
 }
 
 export default App;
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,11 +2,11 @@ import React from 'react';
 import './Header.css';
 
 const Header = () => {
-    return (
-      <header className="site-header">
-        <h1>Historic Denver</h1>
-      </header>
-    );
+  return (
+    <header className="site-header">
+      <h1>Historic Denver</h1>
+    </header>
+  );
 }
 
 export default Header;
